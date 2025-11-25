229af395d0f1acd51d4feaa546da4fde442cbbe5
Kunru Li <kunrul0406@gmail.com>
2025-11-24 18:18:08 -0800

    non-ints to doubles, getters + setters

--- src/main/java/moe/cameo/entities/Entity.java
+++ src/main/java/moe/cameo/entities/Entity.java
@@ -1,14 +1,32 @@
 package moe.cameo.entities;
 
-public class Entity {
-    private float x;
-    private float y;
-    private int max_hp; 
-    private int hp;
+public abstract class Entity {
+    protected double x;
+    protected double y;
+    protected int max_hp; 
+    protected double hp;
+
+    protected double direction; // In degrees
 
     // Getters
     public boolean isAlive() { return this.hp > 0; }
-    public float getX() { return this.x; }
-    public float getY() { return this.y; }
+    public double getX() { return this.x; }
+    public double getY() { return this.y; }
+    public double getDirection() { return this.direction; }
+
+    public double getHP() { return this.hp; }
+    public double getMaxHP() { return this.max_hp; }
+
+    // Setters
+    public void changeHP(int amount) {
+        this.hp += amount;
+
+        // Cap HP
+        if (this.hp < 0) {
+            this.hp = 0;
+        } else if (this.hp > this.max_hp) {
+            this.hp = this.max_hp;
+        }
+    }
 
 }
